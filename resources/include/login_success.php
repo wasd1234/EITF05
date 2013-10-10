a83380e48af0d4d7af26e7e738c70d44abdcab29
jonathanklingberg <jonathan.klingberg@gmail.com>
2013-10-11 00:10:38 +0200

    last commit

--- resources/include/login_success.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (login_check()){
-	echo "<div id='login-success-container'><h2>Welcome!</h2>";
-	echo "<div>You are currently logged in as: <strong>" . $_SESSION['username'] . "</strong></div></div>";
-}else{
-	echo "<div id='login-success-container'><h2>Error!</h2>";
-	echo "<div>You are currently <strong>not</strong> logged in</div></div>";
-}
